9b10aa06692e0916b0c05fa52b0ace4ed7b8a1c0
snurlasova <nurlasova.kg@gmail.com>
2024-01-22 20:53:58 +0600

    added task 1

---
+++ ternary.js
@@ -0,0 +1,19 @@
+let x = 0;
+
+if (Math.random() > 0.5){
+    x =10;
+} else{
+    x = 20;
+}
+
+
+// тоже самое 
+x = Math.random() > 0.5 ? 10 : 20; 
+
+// плохой пример
+
+let age = 23;
+let isAdult = age > 18 ? true : false;
+
+//нужно так 
+let isAdultDoneRight = age > 18;
