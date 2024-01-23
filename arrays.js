5530b0c31cd33cfc8d2fefd2c19d9dd67dda07e7
snurlasova <nurlasova.kg@gmail.com>
2024-01-23 21:59:00 +0600

    added task

--- arrays.js
+++ arrays.js
@@ -1,1 +1,14 @@
-let fruits = ['Apple', 'Banana', 'Orange', 'Lemon'];
+// let fruits = ['Apple', 'Banana', 'Orange', 'Lemon']; 
+
+let alphabet = [];
+
+//добавить в конец массива 
+alphabet.push('г');
+alphabet.push('д');
+alphabet.push('е', 'ж', 'з');
+
+//добавить в начало 
+alphabet.unshift('в');
+alphabet.unshift('a', 'б');
+
+console.log(alphabet);
