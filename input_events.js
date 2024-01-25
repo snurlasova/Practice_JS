ce13de1e52ca4162cd134ea901564c64c014d44a
snurlasova <nurlasova.kg@gmail.com>
2024-01-25 22:38:20 +0600

    added function to input_event.js

--- input_events.js
+++ input_events.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-    let colorInput = document
+    let colorInput = document.querySelector(".color-input");
+    let colorBlock = document.querySelector(".color-block");
+    let clearButton = document.querySelector(".clear-color-button");
 
 
+    function paintBlock() {
+        colorBlock.style.backgroundColor = colorInput.value;
+    }
+     
+    colorInput.addEventListener('input', paintBlock);
+    paintBlock();
+
+    clearButton.addEventListener("click", function(){
+        colorBlock.style.removeProperty("background-color");
+        colorInput.value = "";
+    });
 });
