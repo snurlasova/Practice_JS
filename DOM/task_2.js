fa133c7fdb0bcc6a380dd82d86c9b95d8fc22f0c
snurlasova <nurlasova.kg@gmail.com>
2024-01-26 21:04:13 +0600

    changed function and added style

---
+++ DOM/task_2.js
@@ -0,0 +1,30 @@
+function createStudentCard (student){
+    let studentCard = document.createElement('div');
+
+     let nameStudent = document.createElement('h2');
+     nameStudent.textContext = student.name;
+
+     let ageSpan = document.createElement('span');
+     ageSpan.textContent = 'Возвраст ' + student.age + ' лет';
+
+     studentCard.appendChild(nameStudent);
+     studentCard.appendChild(ageSpan);
+
+     document.body.appendChild(studentCard);
+
+     studentCard.style.backgroundColor = '#00BFFF';
+     studentCard.style.width = 'fit-content';
+     studentCard.style.borderRadius = '15px';
+     studentCard.style.padding = '20px';
+     studentCard.style.margin = '20px';
+     studentCard.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
+     nameStudent.style.marginBottom = '10px';
+
+};
+
+let studentObj = {
+    name: Игорь,
+    age: 17
+};
+
+createStudentCard(studentObj);
